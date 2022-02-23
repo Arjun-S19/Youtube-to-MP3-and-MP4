0169cf6c99936068b26ae2f66e92571a2c62e6ea
Arjun Senthil <68448255+Arjun-S19@users.noreply.github.com>
2022-02-23 14:51:40 -0800

    Update ytmp4.py

--- ytmp4.py
+++ ytmp4.py
@@ -6,4 +6,4 @@ video_url = input("Enter URL: ")
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([video_url])
 
-print("File at -> This PC > Windows(C:) > Users > arjns > PyCharmProjects > Youtube to MP3 & MP4")
+# print(" ") <--- ADD PATH
